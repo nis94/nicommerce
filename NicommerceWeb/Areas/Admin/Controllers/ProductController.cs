f3d2871120797f0b97fa945d9fe5b2ee12c4e976
Nir Bar Levav <nirolol1994@gmail.com>
2025-05-01 11:28:22 +0300

    update category view in product

--- NicommerceWeb/Areas/Admin/Controllers/ProductController.cs
+++ NicommerceWeb/Areas/Admin/Controllers/ProductController.cs
@@ -20,7 +20,7 @@ public class ProductController : Controller
 
     public IActionResult Index()
     {
-        List<Product> productList = _unitOfWork.Product.GetAll().ToList();
+        List<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
         return View(productList);
     }
 
